771e09c47bb4c8b478408c0f337b734075fa1279
Dan Bilauca <dan.bilauca@bitstone.com>
2024-12-25 21:38:23 +0200

    assignment 3

--- Dockerfile
+++ Dockerfile
@@ -3,15 +3,20 @@ WORKDIR /app
 COPY package*.json ./
 RUN npm ci
 
-
 FROM node:20-alpine
+
+ENV APP_PORT=3030
+ENV FE_APP_URL=http:localhost:3000
 ENV ENV="default env"
+
 ENV HOME=/home/customuser
 RUN addgroup -S customgroup && \
     adduser -S -G customgroup customuser && \
-    mkdir -p $HOME/app && \
+    mkdir -p $HOME/app/tmp && \
     chown -R customuser:customgroup $HOME
+
 WORKDIR $HOME/app
+
 COPY --from=builder /app/node_modules ./node_modules
 COPY --chown=customuser:customgroup index.js .
 COPY --chown=customuser:customgroup package*.json .
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3030;
 
@@ -29,8 +30,19 @@ app.get('/posts', (req, res) => {
 
 // Create a new blog post
 app.post('/posts', (req, res) => {
+    
     console.log('POST /posts');
+    
     const newPost = req.body;
+
+    fs.appendFile('./tmp/data.json', JSON.stringify(newPost), (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error writing to file');
+        }
+        console.log('Data written to file');
+    });
+
     blogPosts.push(newPost);
     res.status(201).json(newPost);
 });
@@ -61,6 +73,45 @@ app.delete('/posts/:id', (req, res) => {
     console.log('DELETE /posts/:id');
     blogPosts = blogPosts.filter(p => p.id !== parseInt(req.params.id));
     res.status(204).send();
+});
+
+
+app.get('/read-file', (req, res) => {
+    console.log('GET /read-file');
+    fs.readFile('./quotes.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error reading file');
+        }
+        console.log('Data read from file');
+        res.status(200).send(data);
+    })
+});
+
+app.get('/read', (req, res) => {
+    console.log('GET /read');
+    fs.readFile('./tmp/data.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error reading file');
+        }
+        console.log('Data read from file');
+        res.status(200).send(data);
+    })
+});
+
+app.post('/write', (req, res) => {
+    const payload = req.body;
+    console.log('POST /write');
+    console.log(JSON.stringify(payload));
+    fs.appendFile('./tmp/data.json', JSON.stringify(payload), (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error writing to file');  
+        } 
+        console.log('Data written to file');
+        res.status(200).send('Data written to file');
+    })
 });
 
 app.listen(port, () => {
